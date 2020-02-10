81fe92fcfc4fbec02ae2c24daa6ed408e433b896
AndrewKirpanev <spacewebdeveloper@gmail.com>
2020-02-11 02:10:13 +1000

    firebase connect

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,29 @@ module.exports = {
       options: { prefixes: [`/app/*`] }
     },
     {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        features: {
+          auth: true,
+          database: true,
+          firestore: false,
+          storage: false,
+          messaging: false,
+          functions: true,
+          performance: false
+        },
+        credentials: {
+          apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+          authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+          projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.GATSBY_FIREBASE_APP_ID
+        }
+      }
+    },
+    {
       resolve: "gatsby-plugin-svgr-svgo",
       options: {
         inlineSvgOptions: [
